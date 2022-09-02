e600017def129d0739af2d1c3b6c2c1c0704b513
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2022-09-02 13:46:54 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
-print("hello")
+local GamesTable = {
+    ["Slime Tower Tycoon"] = {ID = 10675066724, Script = "https://raw.githubusercontent.com/LaDamage/Atlas/main/Tycoons/SlimeTowerTycoon.lua"},
+}
+
+for _, games in pairs(GamesTable) do
+    if game.PlaceId == games.ID then
+        print(_)
+        loadstring(game:HttpGet(games.Script))()
+    else
+        game:GetService("Players").LocalPlayer:Kick("This game is not supported by Atlas!")
+    end
+end
