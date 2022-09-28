80acb10523b86d4ae6a3bfe651b29b2504ae0f80
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2022-09-28 15:09:27 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 local GamesTable = {
     ["Slime Tower Tycoon"] = {ID = 10675066724, Script = "https://raw.githubusercontent.com/LaDamage/Atlas/main/Tycoons/SlimeTowerTycoon.lua"},
+    ["Sheep Tycoon"] = {ID = 10903978962, Script = "https://raw.githubusercontent.com/LaDamage/Atlas/main/Tycoons/SheepTycoon.lua"},
 }
 
 for _, games in pairs(GamesTable) do
