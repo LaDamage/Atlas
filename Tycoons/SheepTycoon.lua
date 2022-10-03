0d06f80f7f2660d7356c371304af51e23f9f5b80
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2022-10-03 18:27:33 +0200

    Update SheepTycoon.lua

--- Tycoons/SheepTycoon.lua
+++ Tycoons/SheepTycoon.lua
@@ -3,7 +3,7 @@ for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) d
 local Atlas = loadstring(game:HttpGet("https://raw.githubusercontent.com/VisualRoblox/Roblox/main/UI-Libraries/Visual%20UI%20Library/Source.lua"))()
 local Window = Atlas:CreateWindow("Atlas", "Sheep Tycoon", "Project Atlas", "rbxassetid://10618928818", true, "VisualUIConfigs", "Default")
 
-for _, v in pairs(game:GetService("Workspace").Tycoon.Plot:GetChildren()) do
+for _, v in pairs(game:GetService("Workspace").Tycoon.Tycoons:GetChildren()) do
     if v.Owner.Value == game:GetService("Players").LocalPlayer then
         getgenv().plot = v 
     end
@@ -27,7 +27,7 @@ local CollectDroplets = FarmingSection:CreateToggle("Collect Wool", false, Color
     getgenv().AutoCollect = bool
     while task.wait() do
         if getgenv().AutoCollect then
-            for _, v in pairs(game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Wools:GetChildren()) do
+            for _, v in pairs(game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)].Wools:GetChildren()) do
                 game:GetService("ReplicatedStorage").Signals.RemoteEvents.GetWoolRemote:FireServer(tonumber(v.Name), v.Wool.Position)
             end
         end
@@ -49,8 +49,8 @@ local AutoMerge = FarmingSection:CreateToggle("Auto Merge", false, Color3.fromRG
     getgenv().AutoMerge = bool
     while task.wait(0.1) do
         if getgenv().AutoMerge then
-            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons.Merge.Head, 0)
-            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons.Merge.Head, 1)
+            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"].Merge.Head, 0)
+            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"].Merge.Head, 1)
         end
     end
 end)
@@ -76,8 +76,8 @@ local BuySheeps = BuySection:CreateToggle("Buy Sheep", false, Color3.fromRGB(0, 
     getgenv().BuySheeps = bool
     while task.wait(0.1) do
         if getgenv().BuySheeps then
-            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons[SheepAmount].Head, 0)
-            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons[SheepAmount].Head, 1)
+            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"][SheepAmount].Head, 0)
+            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"][SheepAmount].Head, 1)
         end
     end
 end)
@@ -86,8 +86,8 @@ local BuyRate = BuySection:CreateToggle("Buy Drop Rate", false, Color3.fromRGB(0
     getgenv().BuyRate = bool
     while task.wait(0.1) do
         if getgenv().BuyRate then
-            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons.Upgrade.Head, 0)
-            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons.Upgrade.Head, 1)
+            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"].Upgrade.Head, 0)
+            firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"].Upgrade.Head, 1)
         end
     end
 end)
@@ -115,8 +115,8 @@ local BuyAllSheeps = MiscellaneousSection:CreateToggle("Buy All Sheeps", false, 
         if getgenv().BuyAllSheeps then
             local Buttons = {"Add", "Add3", "Add10", "Add30", "Add50", "Add100"}
             for _, v in pairs(Buttons) do
-                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons[v].Head, 0)
-                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Plot[tostring(plot)].Buttons[v].Head, 1)
+                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"][v].Head, 0)
+                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Tycoon.Tycoons[tostring(plot)]["Buttons_E"][v].Head, 1)
             end
         end
     end
